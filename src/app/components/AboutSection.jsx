307019774af11b90d908694f1b71d8cce887f1f2
dhruvagrawat <dhruvagrawat9@gmail.com>
2024-02-21 23:43:55 +0530

    add

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>React</li>
+        <li>Next.js</li>
+        <li>Tailwind Css</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>MongoDB</li>
+        <li>JavaScript</li>
+        <li>Docker</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bachelor of Technology (B.Tech) in Computer Science and Engineering</li>
+        <li>Higher Secondary Education in PCM + Cs</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Developing front-end Apps with react |IBM</li>
+        <li>Introduction to Cloud Computing| IBM</li>
+        <li>Developing Back-End Apps with Node.js and express | IBM</li>
+        <li>Getting started with Git and Github | IBM </li>
+        <li>Hands-on Introduction to linux Commands and Shell Scripting | IBM</li>
+        <li>Developing AI Application with Python and Flask | IBM</li>
+        <li>IBM Full Stack Software Developer Professional certificate </li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          Enthusiastic software developer with over 6 months of professional experience, dedicated to collaborative teamwork and energized by tackling complex coding challenges. Proven expertise in efficient time management and writing high-quality, optimized code. Participant HACKCBS 5.0 organizes with HLM.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
